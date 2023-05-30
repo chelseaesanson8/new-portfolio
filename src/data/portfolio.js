55a1cc0e570f6cc4a554e81db24e0023545f8575
chelseaesanson8 <chelseaesanson@outlook.com>
2023-05-30 17:01:36 -0400

    Changed porfolio image and links

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,21 +3,21 @@ export default [
         title: 'Pokedex',
         imgUrl: '/assets/pokedex.png',
         stack: ['React', 'CSS', 'JavaScript'],
-        link: 'https://replit.com',
+        link: 'https://replit.com/@chelseasanson/Pokedex-built-with-React',
     },
 
     {
         title: 'Personal Porfolio',
-        imgUrl: '/assets/pokedex.png',
+        imgUrl: '/assets/portfolio.png',
         stack: ['React', 'Tailwind', 'Vite'],
-        link: 'https://thehackershideout.com',
+        link: 'https://new-portfolio-chelseaesanson8.vercel.app/',
     },
 
     {
         title: 'E-Commerce Website',
         imgUrl: '/assets/ecommerce.png',
         stack: ['PHP', 'PostgreSQL', 'HTML', 'CSS'],
-        link: 'https://replit.com',
+        link: 'https://replit.com/@chelseasanson/ecommerce-final-project',
     },
 
 ]
